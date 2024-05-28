bbaa474fa2a91ad7c589bbe5b1cdc252a2171fac
MauricioESerrano <mauryeserrano@hotmail.com>
2024-05-28 13:35:31 -0700

    done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Lab8-Starter
 
 Graceful degradation follows the idea that despite user browser support, the website still functions at highest quality. Service workers work behind the scenes to ensure a seamless and clean experience for user. Both are related in created the best time for our users.
+
+https://mauricioeserrano.github.io/Lab8-Starter/
--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@
 
 const CACHE_NAME = 'lab-8-starter';
 
+// CONSTANTS
 const RECIPE_URLS = [
   'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
   'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
@@ -15,11 +16,8 @@ const RECIPE_URLS = [
 // Installs the service worker. Feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
-      //            added to the cache when the ServiceWorker is installed
-
-      return cache.addAll([]);
+    caches.open(CACHE_NAME).then(function (cache) {    
+      return cache.addAll(RECIPE_URLS);
     })
   );
 });
@@ -31,24 +29,9 @@ self.addEventListener('activate', function (event) {
 
 // Intercept fetch requests and cache them
 self.addEventListener('fetch', function (event) {
-  // We added some known URLs to the cache above, but tracking down every
-  // subsequent network request URL and adding it manually would be very taxing.
-  // We will be adding all of the resources not specified in the intiial cache
-  // list to the cache as they come in.
-  /*******************************/
-  // This article from Google will help with this portion. Before asking ANY
-  // questions about this section, read this article.
-  // NOTE: In the article's code REPLACE fetch(event.request.url) with
-  //       fetch(event.request)
-  // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
-  /*******************************/
-
-
   event.respondWith(
     caches.open(CACHE_NAME).then(function (cache) {
-    // B8. TODO - If the request is in the cache, return with the cached version.
-    //            Otherwise fetch the resource, add it to the cache, and return
-    //            network response.
+   
       return cache.match(event.request).then(function (response) {
         return response || fetch(event.request).then(function (networkResponse) {
           cache.put(event.request, networkResponse.clone());
@@ -57,12 +40,4 @@ self.addEventListener('fetch', function (event) {
       });
     })
   );
-
-  
-
-  // B7. TODO - Respond to the event by opening the cache using the name we gave
-  //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
 });
